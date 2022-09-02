590ba3d1f133a3f86b40ef720cba8e277ed9f4c2
Ioannis Tsimpiris <ioannis@tsimpiris.com>
2022-09-02 20:15:06 +0100

    added threadpool during resizing process - improved error handling

--- batch_image_resizer.py
+++ batch_image_resizer.py
@@ -2,6 +2,9 @@ import os
 import sys
 import glob
 import shutil
+import timeit
+
+from concurrent.futures import ThreadPoolExecutor
 
 import PIL
 from PIL import Image
@@ -10,40 +13,60 @@ from PIL import Image
 def main():
     inputs_dict = ask_inputs()
 
+    starttime = timeit.default_timer()
+
     inputs_dict['Images'] = [f for f in glob.glob(os.path.join(
         inputs_dict['Input folder'], '*.jpg'))]
 
-    print(f'* Images found: {len(inputs_dict["Images"])}')
+    if len(inputs_dict["Images"]) > 0:
+        print(f'* Images found: {len(inputs_dict["Images"])}')
+    else:
+        print('\nNo images found in the given folder. Exiting...')
+        sys.exit()
 
     inputs_dict['Output folder'] = create_output_folder(
         inputs_dict['Input folder'])
 
-    resize_images(inputs_dict['Images'], inputs_dict)
+    n_threads = len(inputs_dict['Images'])
+    with ThreadPoolExecutor(n_threads) as executor:
+        # _ = [executor.submit(resize_image, image, inputs_dict)
+        #      for image in inputs_dict['Images']]
 
-    print('\n\nJob done!')
+        failures = []
+        counter = 0
+        for i, image in enumerate(inputs_dict['Images']):
+            try:
+                executor.submit(resize_image, image, inputs_dict)
+                counter += 1
+            except Exception:
+                failures.append(os.path.basename(image))
+
+        progress(i+1, len(inputs_dict['Images']), suffix='')
+
+    print(
+        f'\n\nJob done in {round(timeit.default_timer() - starttime, 3)} seconds')
+    print(
+        f'Successfully resized images: {counter}/{len(inputs_dict["Images"])}')
+    if len(failures) > 0:
+        print(f'Failed to resize the following images: {", ".join(failures)}')
 
 
-def resize_images(img_lst, inputs_dict):
-    print('\n* Resizing images.....\n')
+def resize_image(image, inputs_dict):
+    img = Image.open(image)
 
-    for i, image in enumerate(img_lst):
-        img = Image.open(image)
+    # images will be rotated if this won't be used
+    img = PIL.ImageOps.exif_transpose(img)
 
-        # images will be rotated if this won't be used
-        img = PIL.ImageOps.exif_transpose(img)
+    wpercent = (inputs_dict['Image width'] / float(img.size[0]))
+    hsize = int((float(img.size[1]) * float(wpercent)))
 
-        wpercent = (inputs_dict['Image width'] / float(img.size[0]))
-        hsize = int((float(img.size[1]) * float(wpercent)))
+    img = img.resize((inputs_dict['Image width'],
+                     hsize), Image.Resampling.LANCZOS)
 
-        img = img.resize(
-            (inputs_dict['Image width'], hsize), Image.Resampling.LANCZOS)
+    output_img_fullpath = os.path.join(
+        inputs_dict['Output folder'], os.path.basename(image))
 
-        output_img_fullpath = os.path.join(
-            inputs_dict['Output folder'], os.path.basename(image))
-
-        img.save(output_img_fullpath)
-
-        progress(i+1, len(img_lst), suffix='')
+    img.save(output_img_fullpath)
 
 
 def ask_inputs():
@@ -86,7 +109,7 @@ def create_output_folder(input_folder):
 
     try:
         os.makedirs(output_folder)
-        print(f'\nOutput folder has been created: {output_folder}')
+        print(f'\nOutput folder has been created: {output_folder}\n')
     except Exception:
         print('\n*** Unable to create output folder... no clue why... will try to continue.... :)')
 
